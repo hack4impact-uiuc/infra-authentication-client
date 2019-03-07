812c03b071e2ecc6a0b336d434871c13c49a51ac
Josh Byster <joshb1050@gmail.com>
2019-03-07 01:00:40 -0600

    Revised

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -1,29 +1,34 @@
-import API_URL from "../components/globalApiUrl.js";
 import Link from "next/link";
 import Router from "next/router";
+
 import Layout from "../components/layout.js";
+import API_URL from "../components/globalApiUrl.js";
 
 export default class extends React.Component {
-  state = { email: "", securityQuestion: "", didLoadQuestion: false, errorMessage: "" ,securityAnswer: ""};
+  state = {
+    email: "",
+    securityQuestion: "",
+    didLoadQuestion: false,
+    errorMessage: "",
+    securityAnswer: ""
+  };
 
   handleChange = event => {
-    const value = event.target.value,
-      name = event.target.name;
-    this.setState({ [name]: value });
+    this.setState({ [event.target.name]: event.target.value });
   };
 
   handleSubmit = event => {
     event.preventDefault();
 
     if (!this.state.didLoadQuestion) {
-      this.setState({  errorMessage: "" });
+      this.setState({ errorMessage: "" });
       fetch(API_URL + "/getSecurityQuestion", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           email: this.state.email
         })
-       })
+      })
         .then(r => r.json())
         .then(resp => {
           console.log(resp);
@@ -31,15 +36,16 @@ export default class extends React.Component {
           if (resp.status != 200) {
             this.setState({ errorMessage: "Email or Password is invalid." });
           } else {
-            this.setState({ securityQuestion: resp.question,didLoadQuestion:true });
+            this.setState({
+              securityQuestion: resp.question,
+              didLoadQuestion: true
+            });
           }
 
           this.setState({ loggingIn: false });
-        }).catch(
-         e =>
-         console.log(e)
-        );
-    } else{
+        })
+        .catch(e => console.log(e));
+    } else {
       // User already got the security question, so now send the response to the server
       this.setState({ errorMessage: "" });
       fetch(API_URL + "/forgotPassword", {
@@ -49,23 +55,23 @@ export default class extends React.Component {
           email: this.state.email,
           answer: this.state.securityAnswer
         })
-       })
+      })
         .then(r => r.json())
         .then(resp => {
           console.log(resp);
           /* returns a JSON object {result: "success"} or {error:""} with 400 status */
           this.setState({ didLoadQuestion: true });
           if (resp.status != 200) {
-            this.setState({ errorMessage: "Answer to security question is wrong" });
+            this.setState({
+              errorMessage: "Answer to security question is wrong"
+            });
           } else {
             this.setState({ errorMessage: resp.message });
           }
 
           this.setState({ loggingIn: false });
-        }).catch(
-         e =>
-         console.log("Error: " + e)
-        );
+        })
+        .catch(e => console.log("Error: " + e));
     }
   };
 
@@ -76,7 +82,6 @@ export default class extends React.Component {
 
         <form onSubmit={this.handleSubmit}>
           <p>{this.state.errorMessage}</p>
-          
 
           <input
             name="email"
@@ -86,21 +91,21 @@ export default class extends React.Component {
             onChange={this.handleChange}
             required
           />
-          
+
           <p>{this.state.securityQuestion}</p>
- 
-          {this.state.didLoadQuestion && <input
-            name="securityAnswer"
-            type="text"
-            value={this.state.securityAnswer}
-            onChange={this.handleChange}
-            required
-          /> } 
+
+          {this.state.didLoadQuestion && (
+            <input
+              name="securityAnswer"
+              type="text"
+              value={this.state.securityAnswer}
+              onChange={this.handleChange}
+              required
+            />
+          )}
           <button name="submit" type="submit">
             {this.state.didLoadQuestion ? "Send PIN" : "Get Security Question"}
           </button>
-
-
         </form>
       </div>
     </Layout>
